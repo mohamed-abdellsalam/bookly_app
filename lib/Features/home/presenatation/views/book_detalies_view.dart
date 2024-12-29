7ba723be67ced322ece650a90373f3b7d66f708e
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2024-12-29 03:00:14 +0200

    add book deatlies view

---
+++ lib/Features/home/presenatation/views/book_detalies_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BookDetaliesView extends StatelessWidget {
+  const BookDetaliesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetaliesView(),
+    );
+  }
+}
