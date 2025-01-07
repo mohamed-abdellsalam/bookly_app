a6c33b8ac62cb80dcdfe347e71af4464f17c1809
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2025-01-07 19:46:43 +0200

    Add fetchSimilarBooks method to HomeRepo for retrieving books by category

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -55,4 +55,28 @@ class HomeRepoImpl implements HomeRepo {
       );
     }
   }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category}) async {
+    try {
+      var data = await apiServics.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&q=subject:programming&Sorting=relevance');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(
+        ServerFailure(
+          errMesage: e.toString(),
+        ),
+      );
+    }
+  }
 }
